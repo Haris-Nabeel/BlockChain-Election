dbcb4bcd8c9bae4ff0d77c7f484c0cf71c1dec59
Haris Nabeel <harisnabeel98@gmail.com>
2021-08-08 21:31:18 +0500

    App Frontend is connected to local blockchain.
    Front end is connected with local blockchain with candidates and account address showing .  Also test file is added to check the contract.

--- src/js/app.js
+++ src/js/app.js
@@ -1,64 +1,102 @@
 App = {
   web3Provider: null,
   contracts: {},
+  account: '0x0',
+  hasVoted: false,
 
-  init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+  init: function() {
+    return App.initWeb3();
+  },
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+  initWeb3: function() {
+    // Is there is an injected web3 instance?
 
-        petsRow.append(petTemplate.html());
+			if (typeof web3 !== 'undefined') {
+
+			ethereum.enable().then(() => {
+			web3 = new Web3(web3.currentProvider);
+
+			});
+
+			} else {
+
+			// If no injected web3 instance is detected, fallback to the TestRPC
+
+			web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));			
+			}
+
+			App.web3Provider=web3.currentProvider;
+
+
+			return App.initContract();
+
+			},
+
+  initContract: function() {
+    $.getJSON("Election.json", function(election) {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.Election = TruffleContract(election);
+      // Connect provider to interact with contract
+      App.contracts.Election.setProvider(App.web3Provider);
+
+      return App.render();
+    });
+  },
+
+  render: function() {
+    var electionInstance;
+    var loader = $("#loader");
+    var content = $("#content");
+
+    loader.show();
+    content.hide();
+
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
       }
     });
 
-    return await App.initWeb3();
+    // Load contract data
+    App.contracts.Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      return electionInstance.candidatesCount();
+    }).then(function(candidatesCount) {
+      var candidatesResults = $("#candidatesResults");
+      candidatesResults.empty();
+
+      var candidatesSelect = $('#candidatesSelect');
+      candidatesSelect.empty();
+
+      for (var i = 1; i <= candidatesCount; i++) {
+        electionInstance.candidates(i).then(function(candidate) {
+          var id = candidate[0];
+          var name = candidate[1];
+          var voteCount = candidate[2];
+
+          // Render candidate Result
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+          candidatesResults.append(candidateTemplate);
+
+          // Render candidate ballot option
+          // var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          // candidatesSelect.append(candidateOption);
+        });
+      }
+    //   return electionInstance.voters(App.account);
+    // }).then(function(hasVoted) {
+    //   // Do not allow a user to vote
+    //   if(hasVoted) {
+    //     $('form').hide();
+    //   }
+      loader.hide();
+      content.show();
+    }).catch(function(error) {
+      console.warn(error);
+    });
   },
-
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
-
-    return App.initContract();
-  },
-
-  initContract: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.bindEvents();
-  },
-
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
-
-  markAdopted: function() {
-    /*
-     * Replace me...
-     */
-  },
-
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
-  }
-
 };
 
 $(function() {
---
+++ test/election.js
@@ -0,0 +1,28 @@
+var Election = artifacts.require("./Election.sol");
+
+contract("Election", function (accounts) {
+
+    it("initializes with two candidates", function () {
+        return Election.deployed().then(function (instance) {
+            return instance.candidatesCount();
+        }).then(function (count) {
+            assert.equal(count, 2);
+        });
+    });
+});
+
+it("it initializes the candidates with the correct values", function() {
+    return Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      return electionInstance.candidates(1);
+    }).then(function(candidate) {
+      assert.equal(candidate[0], 1, "contains the correct id");
+      assert.equal(candidate[1], "Nawaz Sharif", "contains the correct name");
+      assert.equal(candidate[2], 0, "contains the correct votes count");
+      return electionInstance.candidates(2);
+    }).then(function(candidate) {
+      assert.equal(candidate[0], 2, "contains the correct id");
+      assert.equal(candidate[1], "Imran Khan", "contains the correct name");
+      assert.equal(candidate[2], 0, "contains the correct votes count");
+    });
+  });
