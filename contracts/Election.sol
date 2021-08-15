033c80e14fd5dba272c9643bae8c47dea591726c
Haris Nabeel <harisnabeel98@gmail.com>
2021-08-16 01:06:55 +0500

    App done with voting and testing.

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -19,6 +19,9 @@ contract Election{
     // Store Candidates Count
     uint public candidatesCount;
 
+    // voted event
+    event votedEvent(uint indexed _candidateId);
+
     // Adding a Candidate
     function addCandidate(string memory _name) private {
         candidatesCount++;
@@ -27,12 +30,20 @@ contract Election{
 
     // Voting
     function vote(uint _candidateId) public {
+        // checking if voter has already voted
+        require(!voters[msg.sender]);
+
+        //checking that if voter is voting to valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount );
 
         // record that voter has voted
         voters[msg.sender] = true;
         
         // updating candidate vote count
         candidates[_candidateId].voteCount++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 
     // Contructor
--- src/js/app.js
+++ src/js/app.js
@@ -9,28 +9,18 @@ App = {
   },
 
   initWeb3: function() {
-    // Is there is an injected web3 instance?
-
-			if (typeof web3 !== 'undefined') {
-
-			ethereum.enable().then(() => {
-			web3 = new Web3(web3.currentProvider);
-
-			});
-
-			} else {
-
-			// If no injected web3 instance is detected, fallback to the TestRPC
-
-			web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));			
-			}
-
-			App.web3Provider=web3.currentProvider;
-
-
-			return App.initContract();
-
-			},
+    // TODO: refactor conditional
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContract();
+  },
 
   initContract: function() {
     $.getJSON("Election.json", function(election) {
@@ -39,7 +29,26 @@ App = {
       // Connect provider to interact with contract
       App.contracts.Election.setProvider(App.web3Provider);
 
+      App.listenForEvents();
+
       return App.render();
+    });
+  },
+
+  // Listen for events emitted from the contract
+  listenForEvents: function() {
+    App.contracts.Election.deployed().then(function(instance) {
+      // Restart Chrome if you are unable to receive this event
+      // This is a known issue with Metamask
+      // https://github.com/MetaMask/metamask-extension/issues/2393
+      instance.votedEvent({}, {
+        fromBlock: 0,
+        toBlock: 'latest'
+      }).watch(function(error, event) {
+        console.log("event triggered", event)
+        // Reload when a new vote is recorded
+        App.render();
+      });
     });
   },
 
@@ -81,22 +90,35 @@ App = {
           candidatesResults.append(candidateTemplate);
 
           // Render candidate ballot option
-          // var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
-          // candidatesSelect.append(candidateOption);
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidatesSelect.append(candidateOption);
         });
       }
-    //   return electionInstance.voters(App.account);
-    // }).then(function(hasVoted) {
-    //   // Do not allow a user to vote
-    //   if(hasVoted) {
-    //     $('form').hide();
-    //   }
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted) {
+      // Do not allow a user to vote
+      if(hasVoted) {
+        $('form').hide();
+      }
       loader.hide();
       content.show();
     }).catch(function(error) {
       console.warn(error);
     });
   },
+
+  castVote: function() {
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function(instance) {
+      return instance.vote(candidateId, { from: App.account });
+    }).then(function(result) {
+      // Wait for votes to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
+    });
+  }
 };
 
 $(function() {
